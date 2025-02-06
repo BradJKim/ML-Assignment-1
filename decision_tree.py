d2045f85cd04fc51426fc8653ba26d4b7d4d4f4a
BradJKim <bradjkim03@gmail.com>
2025-02-05 23:38:53 -0800

    completed tree

--- decision_tree.py
+++ decision_tree.py
@@ -3,7 +3,7 @@
 # FILENAME: ML-Assignment-1
 # SPECIFICATION: Read the file  contact_lens.csv and output a decision tree
 # FOR: CS 4210- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 38 Minutes
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
@@ -22,17 +22,86 @@ with open('contact_lens.csv', 'r') as csvfile:
   reader = csv.reader(csvfile)
   for i, row in enumerate(reader):
       if i > 0: #skipping the header
-         db.append (row)
-         print(row)
+         db.append(row)
+         #print(row)
 
 #transform the original categorical training features into numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3
 #--> add your Python code here
-# X =
+X = []
+
+# functions to convert feature into numbers
+def convertAge(x):
+  if x == 'Young':
+    return 1
+  if x == 'Prepresbyopic':
+    return 2
+  if x == 'Presbyopic':
+    return 3
+  else:
+    return 1
+
+def convertSpec(x):
+  if x == 'Myope':
+    return 1
+  if x == 'Hypermetrope':
+    return 2
+  else:
+    return 1
+
+def convertAst(x):
+  if x == 'Yes':
+    return 1
+  if x == 'No':
+    return 2
+  else:
+    return 1
+
+def convertTpr(x):
+  if x == 'Normal':
+    return 1
+  if x == 'Reduced':
+    return 2
+  else:
+    return 1
+
+def convertRec(x):
+  if x == 'Yes':
+    return 1
+  if x == 'No':
+    return 2
+  else:
+    return 1
+
+# index to feature conversion mapping
+convertFunc = {
+  0: convertAge,
+  1: convertSpec,
+  2: convertAst,
+  3: convertTpr,
+  4: convertRec,
+}
+
+#For each row in the database,  convert each feature to numerical value and load
+for row in db:
+  updated_feat_row = []
+  
+  for i,feat in enumerate(row[:-1]):
+    changed_val = convertFunc[i](feat)
+    updated_feat_row.append(changed_val)
+  
+  X.append(updated_feat_row)
 
 #transform the original categorical training classes into numbers and add to the vector Y. For instance Yes = 1, No = 2
-#--> addd your Python code here
-# Y =
+#--> add your Python code here
+Y = []
+for row in db:
+  updated_class = []
+  
+  changed_val = convertFunc[len(row)-1](row[-1])
+  updated_class.append(changed_val)
 
+  Y.append(updated_class)
+  
 #fitting the decision tree to the data
 clf = tree.DecisionTreeClassifier(criterion = 'entropy')
 clf = clf.fit(X, Y)
